cc2a54660117d0168a67daa2d71bc1238fa2cd82
Jan Myler <honza.myler@gmail.com>
2015-10-04 12:14:08 +0200

    fix the timezone mixing problem
    resolve #1

--- 10-timeServer.js
+++ 10-timeServer.js
@@ -5,10 +5,14 @@ const params = process.argv.slice(2)
 
 const PORT = Number(params[0])
 
+function zp(num) {
+  return (num < 10 ? '0' : '') + num
+}
+
 // Raw TCP server
 net.createServer(socket => {
-  const now = new Date()
-  let time = `${now.toISOString().slice(0,10)} ${now.getHours()}:${now.getMinutes()}\n`
+  const d = new Date()
+  let time = `${d.getFullYear()}-${zp(d.getMonth() + 1)}-${zp(d.getDate())} ${d.getHours()}:${d.getMinutes()}\n`
   socket.end(time)
 })
 .listen(PORT);
